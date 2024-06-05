2c0225c7ec573e4fa0bceb6a44a0665769b6eb19
ghdtjgus76 <ghdtjgus76@naver.com>
2024-06-06 02:37:56 +0900

    feat: TabPanel 컴포넌트 추가

---
+++ src/ui/components/TabPanel.tsx
@@ -0,0 +1,28 @@
+import { Box, Typography } from '@mui/material';
+import React, { ReactNode } from 'react';
+
+interface TabPanelProps {
+  children: ReactNode;
+  index: number;
+  value: number;
+}
+
+export default function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
