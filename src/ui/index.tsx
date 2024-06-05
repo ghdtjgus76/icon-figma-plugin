e8489f9f74fac5208f58e76d3c18313eb3bac4cc
ghdtjgus76 <ghdtjgus76@naver.com>
2024-06-06 02:20:16 +0900

    feat: mui theme 적용

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider, createTheme } from '@mui/material';
 import App from './App';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2196f3',
+    },
+  },
+});
+
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<App />, document.getElementById('root'));
+  ReactDOM.render(
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>,
+    document.getElementById('root'),
+  );
 });
