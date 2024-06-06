06589961cb898d996ab9aad546ed0c3795422774
ghdtjgus76 <ghdtjgus76@naver.com>
2024-06-06 14:50:27 +0900

    feat: 플러그인에서 UI 메시지 받는 로직 추가

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,1 +1,7 @@
 figma.showUI(__html__);
+
+figma.ui.onmessage = async (message) => {
+  if (message.type === 'setInfo') {
+    await figma.clientStorage.setAsync('userInfo', message.payload);
+  }
+};
