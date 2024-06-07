b87a56cf6b9b239678fea40056871a7e9a24aa00
ghdtjgus76 <ghdtjgus76@naver.com>
2024-06-07 18:28:56 +0900

    refactor: 함수 반환 타입 지정

--- src/plugin/utils.ts
+++ src/plugin/utils.ts
@@ -1,10 +1,13 @@
+import { UserDataType } from '../shared/types';
 import { StorageKey } from './constants';
 
 export const setStorageData = async (key: StorageKey, payload: unknown) => {
   await figma.clientStorage.setAsync(key, payload);
 };
 
-export const getStorageData = async (key: StorageKey) => {
+export const getStorageData = async (
+  key: StorageKey,
+): Promise<UserDataType> => {
   const data = await figma.clientStorage.getAsync(key);
   return data;
 };
