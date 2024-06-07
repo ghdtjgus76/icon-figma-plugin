b602ee967b23e335ab63707ebdf8b42182d65ece
ghdtjgus76 <ghdtjgus76@naver.com>
2024-06-07 18:28:42 +0900

    feat: 아이콘 추출 및 pr 생성 로직 추가

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,11 +1,14 @@
 import { MessageType } from '../shared/constants';
 import { UserDataType } from '../shared/types';
 import { StorageKey } from './constants';
+import githubApi from './github';
 import { getStorageData, setStorageData } from './utils';
 
 figma.showUI(__html__, { height: 360 });
 
-const extractIcon = async () => {
+type SvgIconsType = { [key: string]: string };
+
+const extractIcons = async (): Promise<SvgIconsType> => {
   const currentNodes = figma.currentPage.selection;
   if (!currentNodes.length) {
     console.log('선택된 프레임이 없습니다.');
@@ -21,7 +24,7 @@ const extractIcon = async () => {
     }),
   );
 
-  const svgIconObj = svgIcons.reduce(
+  const svgIconObj: SvgIconsType = svgIcons.reduce(
     (acc, { svg, id }) => ({
       ...acc,
       [id]: svg,
@@ -29,9 +32,46 @@ const extractIcon = async () => {
     {},
   );
 
-  figma.ui.postMessage({
-    type: 'icons',
-    payload: svgIconObj,
+  return svgIconObj;
+};
+
+const createPullRequest = async (svgIcons: SvgIconsType) => {
+  const { githubToken, owner, repo } = await getStorageData(
+    StorageKey.UserData,
+  );
+  const {
+    getLatestCommit,
+    getTreeSha,
+    createTree,
+    createCommit,
+    createBranch,
+    createPullRequest,
+  } = githubApi({
+    githubToken,
+    owner,
+    repo,
+  });
+
+  const latestCommitSha = await getLatestCommit();
+  const baseTreeSha = await getTreeSha(latestCommitSha);
+  const newTreeSha = await createTree(baseTreeSha, [
+    { path: 'packages/icons/icons.json', content: JSON.stringify(svgIcons) },
+  ]);
+  const newCommitSha = await createCommit(
+    'feature: 아이콘 변경사항 반영',
+    newTreeSha,
+    latestCommitSha,
+  );
+  const branchName = await createBranch(
+    `icons-${Date.now().toFixed(6)}`,
+    newCommitSha,
+  );
+
+  await createPullRequest({
+    title: 'Feature: 아이콘 변경사항 반영',
+    body: '피그마 상 아이콘 변경사항 반영했습니다.',
+    head: branchName,
+    base: 'main',
   });
 };
 
@@ -48,6 +88,9 @@ figma.ui.onmessage = async (message) => {
       payload: userData || null,
     });
   }
-};
 
-extractIcon();
+  if (message.type === MessageType.ExtractIcon) {
+    const svgIcons = await extractIcons();
+    createPullRequest(svgIcons);
+  }
+};
